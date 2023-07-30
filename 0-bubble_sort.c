9ce7f9d54a85fbc0f9568703fdcb8694fd38e2e3
fadwa sn <92952003+fadwadwa@users.noreply.github.com>
2023-07-30 02:31:16 +0100

    Create 0-bubble_sort.c

---
+++ 0-bubble_sort.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+/**
+ * bubble_sort - sorts an array of integers in ascending order using the
+ * Bubble Sort algorithm.
+ * @array: a pointer to the array to be sorted.
+ * @size: the size of the array.
+ * Return: void
+ */
+void bubble_sort(int *array, size_t size)
+{
+	int swapped;
+  size_t i, j;
+
+  for (i = 0; i < size - 1; i++)
+  {
+    swapped = 0;
+    for (j = 0; j < size - i - 1; j++)
+    {
+      if (array[j] > array[j + 1])
+      {
+        int temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+
+        printf("After iteration %zu: ", i + 1);
+        for (size_t k = 0; k < size; k++)
+          printf("%d ", array[k]);
+        printf("\n");
+        swapped = 1;
+      }
+    }    
+    if (swapped == 0)
+      break;
+  }
+}
