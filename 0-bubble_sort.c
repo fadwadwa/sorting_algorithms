fb130409fff511c8c85e9365c01e127c57122ff9
fadwadwa <fawda1119@gmail.com>
2023-07-29 18:35:41 -0700

    modif

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,27 +9,26 @@
 void bubble_sort(int *array, size_t size)
 {
 	int swapped;
-  size_t i, j;
+	size_t i, j;
 
-  for (i = 0; i < size - 1; i++)
-  {
-    swapped = 0;
-    for (j = 0; j < size - i - 1; j++)
-    {
-      if (array[j] > array[j + 1])
-      {
-        int temp = array[j];
-        array[j] = array[j + 1];
-        array[j + 1] = temp;
-
-        printf("After iteration %zu: ", i + 1);
-        for (size_t k = 0; k < size; k++)
-          printf("%d ", array[k]);
-        printf("\n");
-        swapped = 1;
-      }
-    }    
-    if (swapped == 0)
-      break;
-  }
+	for (i = 0; i < size - 1; i++)
+	{
+		swapped = 0;
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				int temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				printf("After iteration %zu: ", i + 1);
+				for (size_t k = 0; k < size; k++)
+					printf("%d ", array[k]);
+				printf("\n");
+				swapped = 1;
+			}
+		}
+		if (swapped == 0)
+			break;
+	}
 }
